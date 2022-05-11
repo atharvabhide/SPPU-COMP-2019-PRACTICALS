172caf4ab85a739cfa8c180be010c4893253b609
Atharva Bhide <67187699+atharvabhide@users.noreply.github.com>
2022-05-11 19:24:03 +0530

    Update 12_index_sequential_file.cpp

--- DSA/12_index_sequential_file.cpp
+++ DSA/12_index_sequential_file.cpp
@@ -1,205 +1,442 @@
-#include<iostream>
-#include<fstream>
-#include<stdio.h>
+#include<bits/stdc++.h>
 using namespace std;
-//Employee class Declaration
-class Employee{
- private:
- int code;
- char name[20];
- float salary;
- public:
- void read();
- void display();
- //will return employee code
- int getEmpCode() { return code;}
- //will return employee salary
- int getSalary() { return salary;}
- //will update employee salary
- void updateSalary(float s) { salary=s;}
-};
-//Read employee record
-void Employee::read(){
- cout<<"Enter employee code: ";
- cin>>code;
- cout<<"Enter name: ";
- cin.ignore(1);
- cin.getline(name,20);
- cout<<"Enter salary: ";
- cin>>salary;
-}
-//Display employee record
-void Employee::display()
+
+struct Student
 {
- cout<<code<<" "<<name<<"\t"<<salary<<endl;
-}
-//global declaration
-fstream file;
-//Will delete file when program is being executed
-//because we are create file in append mode
-void deleteExistingFile(){
- remove("EMPLOYEE.DAT");
-}
-//function to append record into file
-void appendToFille(){
- Employee x;
+    int roll_number;
+    string name;
+    string division;
+    string address;
+};
 
- //Read employee record from user
- x.read();
+void add_students()
+{
+    Student s;
+    cout << "Enter the roll number of the student-\n";
+    cin >> s.roll_number;
+    cout << "Enter the name of the student-\n";
+    cin >> s.name;
+    cout << "Enter the division of the student-\n";
+    cin >> s.division;
+    cout << "Enter the address of the student-\n";
+    cin >> s.address;
 
- file.open("EMPLOYEE.DAT",ios::binary|ios::app);
- if(!file){
- cout<<"ERROR IN CREATING FILE\n";
- return;
- }
- //write into file
- file.write((char*)&x,sizeof(x));
- file.close();
- cout<<"Record added sucessfully.\n";
-}
-void displayAll(){
- Employee x;
+    if (1 <= s.roll_number and s.roll_number <= 10)
+    {
+        ofstream write;
+        write.open("1to10.txt", ios::app);
 
- file.open("EMPLOYEE.DAT",ios::binary|ios::in);
- if(!file){
- cout<<"ERROR IN OPENING FILE \n";
- return;
- }
- while(file){
- if(file.read((char*)&x,sizeof(x)))
- if(x.getSalary()>=10000 && x.getSalary()<=20000)
- x.display();
- }
- file.close();
-}
-void searchForRecord(){
- //read employee id
- Employee x;
- int c;
- int isFound=0;
- cout<<"Enter employee code: ";
- cin>>c;
- file.open("EMPLOYEE.DAT",ios::binary|ios::in);
- if(!file){
- cout<<"ERROR IN OPENING FILE \n";
- return;
- }
- while(file){
- if(file.read((char*)&x,sizeof(x))){
- if(x.getEmpCode()==c){
- cout<<"RECORD FOUND\n";
- x.display();
- isFound=1;
- break;
- }
- }
- }
- if(isFound==0){
- cout<<"Record not found!!!\n";
- }
- file.close();
-}
-//Function to increase salary
-void increaseSalary(){
- //read employee id
- Employee x;
- int c;
- int isFound=0;
- float sal;
- cout<<"enter employee code \n";
- cin>>c;
- file.open("EMPLOYEE.DAT",ios::binary|ios::in);
- if(!file){
- cout<<"ERROR IN OPENING FILE \n";
- return;
- }
- while(file){
- if(file.read((char*)&x,sizeof(x))){
- if(x.getEmpCode()==c){
- cout<<"Salary hike? ";
- cin>>sal;
- x.updateSalary(x.getSalary()+sal);
- isFound=1;
- break;
- }
- }
- }
- if(isFound==0){
- cout<<"Record not found!!!\n";
- }
- file.close();
- cout<<"Salary updated successfully."<<endl;
-}
-//Insert record by assuming that records are in
-//ascending order
-void insertRecord(){
- //read employee record
- Employee x;
- Employee newEmp;
+        write << "\n" << s.roll_number;
+        write << "\n" << s.name;
+        write << "\n" << s.division;
+        write << "\n" << s.address;
 
- //Read record to insert
- newEmp.read();
- fstream fin;
- //read file in input mode
- file.open("EMPLOYEE.DAT",ios::binary|ios::in);
- //open file in write mode
- fin.open("TEMP.DAT",ios::binary|ios::out);
- if(!file){
- cout<<"Error in opening EMPLOYEE.DAT file!!!\n";
- return;
- }
- if(!fin){
- cout<<"Error in opening TEMP.DAT file!!!\n";
- return;
- }
- while(file){
- if(file.read((char*)&x,sizeof(x))){
- if(x.getEmpCode()>newEmp.getEmpCode()){
- fin.write((char*)&newEmp, sizeof(newEmp));
- }
- //no need to use else
- fin.write((char*)&x, sizeof(x));
- }
- }
- fin.close();
- file.close();
+        write.close();
+    }
+    if (11 <= s.roll_number and s.roll_number <= 20)
+    {
+        ofstream write;
+        write.open("11to20.txt", ios::app);
 
- rename("TEMP.DAT","EMPLOYEE.DAT");
- remove("TEMP.DAT");
- cout<<"Record inserted successfully."<<endl;
+        write << "\n" << s.roll_number;
+        write << "\n" << s.name;
+        write << "\n" << s.division;
+        write << "\n" << s.address;
+
+        write.close();
+    }
+    if (21 <= s.roll_number and s.roll_number <= 30)
+    {
+        ofstream write;
+        write.open("21to30.txt", ios::app);
+
+        write << "\n" << s.roll_number;
+        write << "\n" << s.name;
+        write << "\n" << s.division;
+        write << "\n" << s.address;
+
+        write.close();
+    }
+    if (31 <= s.roll_number and s.roll_number <= 40)
+    {
+        ofstream write;
+        write.open("31to40.txt", ios::app);
+
+        write << "\n" << s.roll_number;
+        write << "\n" << s.name;
+        write << "\n" << s.division;
+        write << "\n" << s.address;
+
+        write.close();
+    }
+    if (41 <= s.roll_number and s.roll_number <= 50)
+    {
+        ofstream write;
+        write.open("41to50.txt", ios::app);
+
+        write << "\n" << s.roll_number;
+        write << "\n" << s.name;
+        write << "\n" << s.division;
+        write << "\n" << s.address;
+
+        write.close();
+    }
 }
+
+void print(Student s)
+{
+    cout << "Roll number: " << s.roll_number << endl;
+    cout << "Name: " << s.name << endl;
+    cout << "Division: " << s.division << endl;
+    cout << "Address: " << s.address << endl;
+}
+
+void display_students()
+{
+    Student s1;
+    ifstream read;
+    read.open("1to10.txt");
+    while (!read.eof())
+    {
+        read >> s1.roll_number;
+        read >> s1.name;
+        read >> s1.division;
+        read >> s1.address;
+
+        print(s1);
+    }
+    read.close();
+
+    cout << endl;
+
+    Student s2;
+    read.open("11to20.txt");
+    while (!read.eof())
+    {
+        read >> s2.roll_number;
+        read >> s2.name;
+        read >> s2.division;
+        read >> s2.address;
+
+        print(s2);
+    }
+    read.close();
+
+    cout << endl;
+
+    Student s3;
+    read.open("21to30.txt");
+    while (!read.eof())
+    {
+        read >> s3.roll_number;
+        read >> s3.name;
+        read >> s3.division;
+        read >> s3.address;
+
+        print(s3);
+    }
+    read.close();
+
+    cout << endl;
+
+    Student s4;
+    read.open("31to40.txt");
+    while (!read.eof())
+    {
+        read >> s4.roll_number;
+        read >> s4.name;
+        read >> s4.division;
+        read >> s4.address;
+
+        print(s4);
+    }
+    read.close();
+
+    cout << endl;
+
+    Student s5;
+    read.open("41to50.txt");
+    while (!read.eof())
+    {
+        read >> s5.roll_number;
+        read >> s5.name;
+        read >> s5.division;
+        read >> s5.address;
+
+        print(s5);
+    }
+    read.close();
+
+    cout << endl;
+}
+
+int search_students(int roll_number)
+{
+    if (1 <= roll_number and roll_number <= 10)
+    {
+        Student s;
+        ifstream read;
+        read.open("1to10.txt");
+        while (!read.eof())
+        {
+            read >> s.roll_number;
+            read >> s.name;
+            read >> s.division;
+            read >> s.address;
+            if (s.roll_number == roll_number)
+            {
+                cout << "Student's record found!" << endl;
+                return roll_number;
+            }
+        }
+
+        read.close();
+    }
+    if (11 <= roll_number and roll_number <= 20)
+    {
+        Student s;
+        ifstream read;
+        read.open("11to20.txt");
+        while (!read.eof())
+        {
+            read >> s.roll_number;
+            read >> s.name;
+            read >> s.division;
+            read >> s.address;
+            if (s.roll_number == roll_number)
+            {
+                cout << "Student's record found!" << endl;
+                return roll_number;
+            }
+        }
+
+        read.close();
+    }
+    if (21 <= roll_number and roll_number <= 30)
+    {
+        Student s;
+        ifstream read;
+        read.open("21to30.txt");
+        while (!read.eof())
+        {
+            read >> s.roll_number;
+            read >> s.name;
+            read >> s.division;
+            read >> s.address;
+            if (s.roll_number == roll_number)
+            {
+                cout << "Student's record found!" << endl;
+                return roll_number;
+            }
+        }
+
+        read.close();
+    }
+    if (31 <= roll_number and roll_number <= 40)
+    {
+        Student s;
+        ifstream read;
+        read.open("31to40.txt");
+        while (!read.eof())
+        {
+            read >> s.roll_number;
+            read >> s.name;
+            read >> s.division;
+            read >> s.address;
+            if (s.roll_number == roll_number)
+            {
+                cout << "Student's record found!" << endl;
+                return roll_number;
+            }
+        }
+
+        read.close();
+    }
+    if (41 <= roll_number and roll_number <= 50)
+    {
+        Student s;
+        ifstream read;
+        read.open("41to50.txt");
+        while (!read.eof())
+        {
+            read >> s.roll_number;
+            read >> s.name;
+            read >> s.division;
+            read >> s.address;
+            if (s.roll_number == roll_number)
+            {
+                cout << "Student's record found!" << endl;
+                return roll_number;
+            }
+        }
+
+        read.close();
+    }
+
+    return -1;
+}
+
+void delete_students(int roll_number)
+{
+    roll_number = search_students(roll_number);
+
+    if (roll_number == -1)
+    {
+        cout << "Student record to be deleted is not present in the file!" << endl;
+        return;
+    }
+    
+    if (1 <= roll_number and roll_number <= 10)
+    {
+        Student s;
+        ifstream read;
+        ofstream write;
+        read.open("1to10.txt");
+        write.open("temp.txt");
+        while (!read.eof())
+        {
+            read >> s.roll_number;
+            read >> s.name;
+            read >> s.division;
+            read >> s.address;
+            if (s.roll_number != roll_number)
+            {
+                write << "\n" << s.roll_number;
+                write << "\n" << s.name;
+                write << "\n" << s.division;
+                write << "\n" << s.address;
+            }
+        }
+        read.close();
+        write.close();
+        remove("1to10.txt");
+        rename("temp.txt", "1to10.txt");
+    }
+    if (11 <= roll_number and roll_number <= 20)
+    {
+        Student s;
+        ifstream read;
+        ofstream write;
+        read.open("11to20.txt");
+        write.open("temp.txt");
+        while (!read.eof())
+        {
+            read >> s.roll_number;
+            read >> s.name;
+            read >> s.division;
+            read >> s.address;
+            if (s.roll_number != roll_number)
+            {
+                write << "\n" << s.roll_number;
+                write << "\n" << s.name;
+                write << "\n" << s.division;
+                write << "\n" << s.address;
+            }
+        }
+        read.close();
+        write.close();
+        remove("11to20.txt");
+        rename("temp.txt", "11to20.txt");
+    }
+    if (21 <= roll_number and roll_number <= 30)
+    {
+        Student s;
+        ifstream read;
+        ofstream write;
+        read.open("21to30.txt");
+        write.open("temp.txt");
+        while (!read.eof())
+        {
+            read >> s.roll_number;
+            read >> s.name;
+            read >> s.division;
+            read >> s.address;
+            if (s.roll_number != roll_number)
+            {
+                write << "\n" << s.roll_number;
+                write << "\n" << s.name;
+                write << "\n" << s.division;
+                write << "\n" << s.address;
+            }
+        }
+        read.close();
+        write.close();
+        remove("21to30.txt");
+        rename("temp.txt", "21to30.txt");
+    }
+    if (31 <= roll_number and roll_number <= 40)
+    {
+        Student s;
+        ifstream read;
+        ofstream write;
+        read.open("31to40.txt");
+        write.open("temp.txt");
+        while (!read.eof())
+        {
+            read >> s.roll_number;
+            read >> s.name;
+            read >> s.division;
+            read >> s.address;
+            if (s.roll_number != roll_number)
+            {
+                write << "\n" << s.roll_number;
+                write << "\n" << s.name;
+                write << "\n" << s.division;
+                write << "\n" << s.address;
+            }
+        }
+        read.close();
+        write.close();
+        remove("31to40.txt");
+        rename("temp.txt", "31to40.txt");
+    }
+    if (41 <= roll_number and roll_number <= 50)
+    {
+        Student s;
+        ifstream read;
+        ofstream write;
+        read.open("1to10.txt");
+        write.open("temp.txt");
+        while (!read.eof())
+        {
+            read >> s.roll_number;
+            read >> s.name;
+            read >> s.division;
+            read >> s.address;
+            if (s.roll_number != roll_number)
+            {
+                write << "\n" << s.roll_number;
+                write << "\n" << s.name;
+                write << "\n" << s.division;
+                write << "\n" << s.address;
+            }
+        }
+        read.close();
+        write.close();
+        remove("41to50.txt");
+        rename("temp.txt", "41to50.txt");
+    }
+}
+
 int main()
 {
- char ch;
- //if required then only remove the file
- deleteExistingFile();
- do{
- int n;
- cout<<"ENTER CHOICE\n"<<"1.ADD AN EMPLOYEE\n"<<"2.DISPLAY\n"<<"3.SEARCH\n"<<"4.INCREASE SALARY\n"<<"5.INSERT RECORD\n";
- cout<<"Make a choice: ";
- cin>>n;
- switch(n){
- case 1:
- appendToFille();
- break;
- case 2 :
- displayAll();
- break;
- case 3:
- searchForRecord();
- break;
- case 4:
- increaseSalary();
- break;
- case 5:
- insertRecord();
- break;
- default :
- cout<<"Invalid Choice\n";
- }
- cout<<"Do you want to continue ? : ";
- cin>>ch;
- }while(ch=='Y'||ch=='y');
+    add_students();
+    add_students();
+    add_students();
+    add_students();
+    add_students();
+    add_students();
 
- return 0;
+    display_students();
+
+    int found = search_students(34);
+    if (found == -1) 
+    {
+        cout << "Record not found!" << endl;
+        cout << endl;
+    }
+
+    delete_students(7);
+    display_students();
+    return 0;
 }
