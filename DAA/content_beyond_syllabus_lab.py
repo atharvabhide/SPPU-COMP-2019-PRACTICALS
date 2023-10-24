00329a211bc5a3bf78a55fdff8f0764552494351
Atharva Bhide <67187699+atharvabhide@users.noreply.github.com>
2023-10-24 13:21:58 +0530

    Update content_beyond_syllabus_lab.py

--- DAA/content_beyond_syllabus_lab.py
+++ DAA/content_beyond_syllabus_lab.py
@@ -1,53 +1,79 @@
-def is_safe(board, row, col, n):
-    for i in range(row):
-        if board[i][col] == 1:
-            return False
-        
-    i, j = row, col
-    while i >= 0 and j >= 0:
-        if board[i][j] == 1:
-            return False
-        i -= 1
-        j -= 1
-
-    i, j = row, col
-    while i >= 0 and j < n:
-        if board[i][j] == 1:
-            return False
-        i -= 1
-        j += 1
-
+N = 8
+ 
+def printSolution(board):
+    for i in range(N):
+        for j in range(N):
+            if board[i][j] == 1:
+                print("Q", end = " ")
+            else:
+                print(".", end = " ")
+        print()
+ 
+def isSafe(row, col, slashCode, backslashCode, 
+           rowLookup, slashCodeLookup, 
+                       backslashCodeLookup):
+    if (slashCodeLookup[slashCode[row][col]] or
+        backslashCodeLookup[backslashCode[row][col]] or
+        rowLookup[row]):
+        return False
     return True
 
-def solve_n_queens_bnb(n):
-    board = [[0 for _ in range(n)] for _ in range(n)]
-    solutions = []
+def solveNQueensUtil(board, col, slashCode, backslashCode, 
+                     rowLookup, slashCodeLookup,
+                     backslashCodeLookup):
+    if(col >= N):
+        return True
+    for i in range(N):
+        if(isSafe(i, col, slashCode, backslashCode, 
+                  rowLookup, slashCodeLookup,
+                  backslashCodeLookup)):
+        
+            board[i][col] = 1
+            rowLookup[i] = True
+            slashCodeLookup[slashCode[i][col]] = True
+            backslashCodeLookup[backslashCode[i][col]] = True
+             
+            if(solveNQueensUtil(board, col + 1, 
+                                slashCode, backslashCode, 
+                                rowLookup, slashCodeLookup, 
+                                backslashCodeLookup)):
+                return True
+             
+            board[i][col] = 0
+            rowLookup[i] = False
+            slashCodeLookup[slashCode[i][col]] = False
+            backslashCodeLookup[backslashCode[i][col]] = False
+         
+    return False
 
-    def print_solution(board):
-        for row in board:
-            print("".join(["Q" if col == 1 else "." for col in row]))
-        print()
+def solveNQueens():
+    board = [[0 for i in range(N)] 
+                for j in range(N)]
+  
+    slashCode = [[0 for i in range(N)] 
+                    for j in range(N)]
+    backslashCode = [[0 for i in range(N)] 
+                        for j in range(N)]
+     
+    rowLookup = [False] * N
+     
+    x = 2 * N - 1
+    slashCodeLookup = [False] * x
+    backslashCodeLookup = [False] * x
+     
+    for rr in range(N):
+        for cc in range(N):
+            slashCode[rr][cc] = rr + cc
+            backslashCode[rr][cc] = rr - cc + 7
+     
+    if(solveNQueensUtil(board, 0, slashCode, backslashCode, 
+                        rowLookup, slashCodeLookup, 
+                        backslashCodeLookup) == False):
+        print("Solution does not exist")
+        return False
+         
+    printSolution(board)
+    return True
+ 
 
-    def solve(row):
-        if row == n:
-            solutions.append([row[:] for row in board])
-            return
-
-        for col in range(n):
-            if is_safe(board, row, col, n):
-                board[row][col] = 1
-                solve(row + 1)
-                board[row][col] = 0
-
-    solve(0)
-
-    return solutions
-
-n = 8
-solutions = solve_n_queens_bnb(n)
-
-for i, solution in enumerate(solutions):
-    print(f"Solution {i + 1}:")
-    for row in solution:
-        print("".join(["Q" if col == 1 else "." for col in row]))
-    print("-" * 40)
+solveNQueens()
