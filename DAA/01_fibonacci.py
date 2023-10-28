6742f76cb51f8d31ec22d9d0c95a1a5b809d900c
Atharva Bhide <67187699+atharvabhide@users.noreply.github.com>
2023-10-28 23:49:37 +0530

    Update 01_fibonacci.py

--- DAA/01_fibonacci.py
+++ DAA/01_fibonacci.py
@@ -1,20 +1,30 @@
-def fibRecursive(n):
+def fibonacci_iter(n):
+    if n < 0:
+        return -1, 1
     if n == 0 or n == 1:
-        return 1
-    return fibRecursive(n - 1) + fibRecursive(n - 2)
-
-def fibIterative(n):
-    if n == 0 or n == 1:
-        return 1
-    a = 1
+        return n, 1
+    steps = 0
+    a = 0
     b = 1
-    for i in range(2, n + 1):
+    for i in range(2, n+1):
         c = a + b
         a = b
         b = c
-    return c
+        steps += 1
+    return c, steps+1
 
-if __name__ == "__main__":
-    n = int(input("Enter a number-\n"))
-    print("n'th fibonacci number by Recursive method: ", fibRecursive(n))
-    print("n'th fibonacci number by Iterative method: ", fibIterative(n))
+def fibonacci_recur(n):
+    if n < 0:
+        return -1, 1
+    if n == 0 or n == 1:
+        return n, 1
+    fib1, steps1 = fibonacci_recur(n-1)
+    fib2, steps2 = fibonacci_recur(n-2)
+    return fib1 + fib2, steps1 + steps2 + 1
+
+if __name__ == '__main__':
+    n = int(input("Enter a number: "))
+    print("Iterative:", fibonacci_iter(n)[0])
+    print("Steps:", fibonacci_iter(n)[1])
+    print("Recursive:", fibonacci_recur(n)[0])
+    print("Steps:", fibonacci_recur(n)[1])
