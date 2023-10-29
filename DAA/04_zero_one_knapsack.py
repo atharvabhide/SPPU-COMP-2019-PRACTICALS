8596a251a6dcde25c957b39984a94f994ad9b25a
Atharva Bhide <67187699+atharvabhide@users.noreply.github.com>
2023-10-29 16:10:25 +0530

    Update 04_zero_one_knapsack.py

--- DAA/04_zero_one_knapsack.py
+++ DAA/04_zero_one_knapsack.py
@@ -1,31 +1,31 @@
-def knapsack_01(values, weights, capacity):
-    n = len(values)
-    dp = [[0] * (capacity + 1) for _ in range(n + 1)]
+def knapsack_01(n, values, weights, W):
+    dp = [[0] * (W+1) for _ in range(n+1)]
 
-    for i in range(n + 1):
-        for w in range(capacity + 1):
+    for i in range(n+1):
+        for w in range(W+1):
             if i == 0 or w == 0:
                 dp[i][w] = 0
-            elif weights[i - 1] <= w:
-                dp[i][w] = max(dp[i - 1][w], dp[i - 1][w - weights[i - 1]] + values[i - 1])
+            elif weights[i-1] <= w:
+                dp[i][w] = max(dp[i-1][w], dp[i-1][w-weights[i-1]] + values[i-1])
             else:
-                dp[i][w] = dp[i - 1][w]
-
+                dp[i][w] = dp[i-1][w]
+    
     selected_items = []
-    i, w = n, capacity
+    i, w = n, W
     while i > 0 and w > 0:
-        if dp[i][w] != dp[i - 1][w]:
-            selected_items.append(i - 1)
-            w -= weights[i - 1]
+        if dp[i][w] != dp[i-1][w]:
+            selected_items.append(i-1)
+            w -= weights[i-1]
         i -= 1
-
-    return dp[n][capacity], selected_items
+    
+    return dp[n][W], selected_items
 
 if __name__ == "__main__":
+    n = 3
     values = [60, 100, 120]
     weights = [10, 20, 30]
-    capacity = 50
+    W = 50
 
-    max_value, selected_items = knapsack_01(values, weights, capacity)
+    max_value, selected_items = knapsack_01(n, values, weights, W)
     print("Maximum value:", max_value)
     print("Selected items:", selected_items)
